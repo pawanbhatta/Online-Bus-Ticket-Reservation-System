c0a70f222b461992fe3ead7699bcc35de8a7d2f1
= <=>
2020-04-18 10:30:46 +0545

    Operator and bus images were added

--- resources/views/admin/schedules/add-schedule.blade.php
+++ resources/views/admin/schedules/add-schedule.blade.php
@@ -127,7 +127,7 @@
                                 </div>
                             </div>
                       </fieldset>
-        </div>
+                    </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
           <button type="submit" class="btn btn-primary">Register Schedule</button>
